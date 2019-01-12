57e25b589987824d2ebe23b6cfaa3619497ea855
Tony Collen <tony@halogenlabs.com>
2019-01-12 10:40:05 -0600

    Init repo

---
+++ lib/muf_data.ex
@@ -0,0 +1,18 @@
+defmodule MufData do
+  @moduledoc """
+  Documentation for MufData.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MufData.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/muf_data_test.exs
@@ -0,0 +1,8 @@
+defmodule MufDataTest do
+  use ExUnit.Case
+  doctest MufData
+
+  test "greets the world" do
+    assert MufData.hello() == :world
+  end
+end
