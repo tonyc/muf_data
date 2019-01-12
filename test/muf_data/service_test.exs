f4348f829fba6bb86d32c1aa28d6828b3df9d05a
Tony Collen <tony@halogenlabs.com>
2019-01-12 11:56:39 -0600

    Super basic test

--- test/muf_data/service_test.exs
+++ test/muf_data/service_test.exs
@@ -2,6 +2,14 @@ defmodule MufData.ServiceTest do
   use ExUnit.Case
 
   test "picks out the stations key from the returned JSON" do
-    assert true == true
+    {:ok, stations} = MufData.Service.fetch_stations()
+    
+    assert Enum.count(stations) == 78
+
+    wallops = stations
+    |> Enum.find(fn(s) -> s["name"] == "Wallops" end)
+
+    assert wallops["muf_mhz"] == 19.012
+    assert wallops["timestamp"] == "2019-01-12T17:10:00Z"
   end
 end
