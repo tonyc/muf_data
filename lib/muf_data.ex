f0a0d68b4bdb3c8d236c5a3776dae70be4887cda
Tony Collen <tony@halogenlabs.com>
2019-01-12 11:05:12 -0600

    Fetch and parse JSON response from web server

--- lib/muf_data.ex
+++ lib/muf_data.ex
@@ -1,5 +1,16 @@
 defmodule MufData do
   def fetch_stations() do
-    with {:ok, result} <- HTTPoison.get
+    with {:ok, result} <- HTTPoison.get(full_url()),
+         {:ok, stations} <- Jason.decode(result.body) do
+      {:ok, stations["stations"]}
+    end
+  end
+
+  defp full_url() do
+    "#{base_url()}/muf.json"
+  end
+
+  defp base_url() do
+    Application.get_env(:muf_data, :base_url)
   end
 end
