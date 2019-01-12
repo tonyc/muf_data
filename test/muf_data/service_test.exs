f4e4af7d246183ad5c23c701e52857c467b4f717
Tony Collen <tony@halogenlabs.com>
2019-01-12 11:56:49 -0600

    Format code

--- test/muf_data/service_test.exs
+++ test/muf_data/service_test.exs
@@ -3,11 +3,12 @@ defmodule MufData.ServiceTest do
 
   test "picks out the stations key from the returned JSON" do
     {:ok, stations} = MufData.Service.fetch_stations()
-    
+
     assert Enum.count(stations) == 78
 
-    wallops = stations
-    |> Enum.find(fn(s) -> s["name"] == "Wallops" end)
+    wallops =
+      stations
+      |> Enum.find(fn s -> s["name"] == "Wallops" end)
 
     assert wallops["muf_mhz"] == 19.012
     assert wallops["timestamp"] == "2019-01-12T17:10:00Z"
