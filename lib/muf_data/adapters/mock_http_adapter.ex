3c96202c940f32d9f797fa7024c45ce6c5c741ce
Tony Collen <tony@halogenlabs.com>
2019-01-12 12:02:53 -0600

    Switch pattern match = to <-

--- lib/muf_data/adapters/mock_http_adapter.ex
+++ lib/muf_data/adapters/mock_http_adapter.ex
@@ -1,7 +1,7 @@
 defmodule MufData.Adapters.MockHttpAdapter do
   def fetch_stations() do
     with {:ok, file_contents} <- File.read("test/sample_data/muf.json"),
-         {:ok, stations} = Jason.decode(file_contents) do
+         {:ok, stations} <- Jason.decode(file_contents) do
       {:ok, stations["stations"]}
     end
   end
